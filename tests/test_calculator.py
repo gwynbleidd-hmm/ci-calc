2879caf957932a9d6b9aa5b2e9ef4ef50810cbd7
Harith Unnikrishnan <harith.unnikrishnan@mountblue.tech>
2020-04-24 11:13:28 +0530

    Fix test flake8

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,9 +1,11 @@
 from calculator import add, subtract
 
+
 def test_add():
     out = add(3, 4)
     expected_out = 7
     assert out == expected_out
+
 
 def test_subtract():
     out = subtract(9, 1)
